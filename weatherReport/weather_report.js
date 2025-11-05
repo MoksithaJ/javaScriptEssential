4e6e933757779ad7b33dd59255f0383e29ffb432
moksithaj <e22159@eng.pdn.ac.lk>
2025-11-05 09:08:50 -0500

    getweatherwithfetchapi

---
+++ weatherReport/weather_report.js
@@ -0,0 +1,36 @@
+
+function showweatherDetails(event) {
+    event.preventDefault();
+    const city = document.getElementById('city').value;
+    const lon = document.getElementById('lon').value;
+    const lat = document.getElementById('lat').value;
+    const apiKey = //'YOUR_API_KEY_'; 
+    let apiUrl = "";
+    if (lat && lon) {
+      apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+    } else if (city) {
+      apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    } else {
+      document.getElementById('weatherInfo').innerHTML = "<p>Please enter a city or coordinates.</p>";
+      return;
+    }
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      const weatherInfo = document.getElementById('weatherInfo');
+      weatherInfo.innerHTML = `
+            <h2>Weather in ${data.name}</h2>
+            <p>Temperature: ${data.main.temp} &#8451;</p>
+            <p>Weather: ${data.weather[0].description}</p>
+            <p>Humidity: ${data.main.humidity}%</p>
+            <p>Wind Speed: ${data.wind.speed} m/s</p>
+          `;
+    })
+    .catch(error => {
+        console.error('Error fetching weather:', error);
+        const weatherInfo = document.getElementById('weatherInfo');
+        weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
+      });
+}
+
+document.getElementById('weatherForm').addEventListener('submit',showweatherDetails );
